181c0d8646a21a8673a2af0b11541ca9005c9784
mb32154 <study.planner.pro1@gmail.com>
2026-01-05 23:04:40 +0330

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Middleware
 app.use(cors());
@@ -206,4 +207,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
     console.log(`Admin panel will be available at http://localhost:${PORT}/admin.html`);
+
 });
